5fe479d6665da670d892c7c6f130b912c5082133
Sebastian gutierrez <sebasgut38@gmail.com>
2024-09-16 12:14:35 -0500

    se crean modelos de datos

---
+++ src/main/java/com/example/SURATECH5/MODELOS/SignoVital.java
@@ -0,0 +1,10 @@
+package com.example.SURATECH5.MODELOS;
+
+import java.time.LocalDate;
+
+public class SignoVital {
+    String nombre;
+    Double valor;
+    LocalDate fechaTomo;
+    String descripcion;
+}
